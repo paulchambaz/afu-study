2ac081fab1e8c5da8c3adedffaf0c27567f06992
Frédéric Li Combeau <71965677+Elta305@users.noreply.github.com>
2025-02-12 22:37:55 +0100

    Added Pendulum

---
+++ afu/environments/pendulum.py
@@ -0,0 +1,19 @@
+from gymnasium.envs.classic_control.pendulum import PendulumEnv
+from gymnasium.envs.registration import register
+
+
+class PendulumEnvStudy(PendulumEnv):
+    def set_state(
+        self, theta, theta_dot
+    ):
+        self.state = (
+            theta,
+            theta_dot
+        )
+
+
+register(
+    id="PendulumStudy-v0",
+    entry_point="afu.environments.pendulum:PendulumEnvStudy",
+    max_episode_steps=500,
+)
