1d663d426fc802fd99c89df5d80d023ce70fbf5d
Paul Chambaz <paul.chambaz@tutanota.com>
2025-02-12 20:21:33 +0100

    start of afu perrin

---
+++ afu/agents/afu_perrin.py
@@ -0,0 +1,12 @@
+from afu_rljax.algorithm import AFU
+import jax
+
+class AFUPerrin:
+    def __init__(self, params: dict) -> None:
+        pass
+
+    def select_action(self, state: np.ndarray, evaluation: bool = False) -> np.ndarray:
+        pass
+
+    def update(self):
+        pass
