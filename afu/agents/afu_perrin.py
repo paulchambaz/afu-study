bbaee049b8b47e402fd2c665811834fe594c977d
Paul Chambaz <paul.chambaz@tutanota.com>
2025-02-12 20:21:45 +0100

    lint

--- afu/agents/afu_perrin.py
+++ afu/agents/afu_perrin.py
@@ -1,11 +1,14 @@
 from afu_rljax.algorithm import AFU
 import jax
 
+
 class AFUPerrin:
     def __init__(self, params: dict) -> None:
         pass
 
-    def select_action(self, state: np.ndarray, evaluation: bool = False) -> np.ndarray:
+    def select_action(
+        self, state: np.ndarray, evaluation: bool = False
+    ) -> np.ndarray:
         pass
 
     def update(self):
