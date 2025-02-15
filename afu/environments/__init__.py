8b25b7a10231ef53e1855df5e7e1682c3abc7c44
Frédéric LI COMBEAU <frederic.li-combeau-longuet@ens.usvq.fr>
2025-02-15 22:35:02 +0100

    Other implementation of AFU, compute_target not working yet, added bipedal walker

--- afu/environments/__init__.py
+++ afu/environments/__init__.py
@@ -2,7 +2,8 @@ from . import base
 from . import cartpole
 from . import mountaincar
 from . import pendulum
+from . import bipedalwalker
 from . import utils
 from . import wrappers
 
-__all__ = ["base", "cartpole", "pendulum", "utils", "wrappers", "mountaincar"]
+__all__ = ["base", "bipedalwalker", "cartpole", "pendulum", "utils", "wrappers", "mountaincar"]
