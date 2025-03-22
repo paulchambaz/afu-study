c2c1d3f8995b2462948da3d68663ef1a06f5e197
Paul Chambaz <paul.chambaz@tutanota.com>
2025-03-22 18:02:09 +0100

    off policy was not individual run

--- afu/experiments/off_policy.py
+++ afu/experiments/off_policy.py
@@ -4,60 +4,54 @@ from tqdm import tqdm  # type: ignore
 
 
 class OffPolicy(Experiment):
-    def run(self, j, shared_results, results_lock):
+    def run(self, i, shared_results, results_lock):
         obs_scale_factor = 0.5
 
-        for i in range(self.params["n"]):
-            training_steps = 0
-            agent = self.algo(self.hyperparameters)
+        training_steps = 0
+        agent = self.algo(self.hyperparameters)
 
-            # progress = tqdm(
-            #     range(self.params["total_steps"]),
-            #     desc=f"Training {i}/{self.params['n']}",
-            # )
+        progress = tqdm(
+            range(self.params.total_steps),
+            desc=f"Training {i}/{self.params.n}",
+        )
 
-            while training_steps < self.params["total_steps"]:
-                state, _ = agent.train_env.reset()
+        while training_steps < self.params.total_steps:
+            state, _ = agent.train_env.reset()
 
-                obs_low, obs_high = agent.train_env.unwrapped.get_observation_space()
-                random_state = np.random.uniform(
-                    low=obs_low * obs_scale_factor, high=obs_high * obs_scale_factor
-                )
-                agent.train_env.unwrapped.set_state(*random_state)
-                state = agent.train_env.unwrapped.get_obs()
+            obs_low, obs_high = agent.train_env.unwrapped.get_observation_space()
+            random_state = np.random.uniform(
+                low=obs_low * obs_scale_factor, high=obs_high * obs_scale_factor
+            )
+            agent.train_env.unwrapped.set_state(*random_state)
+            state = agent.train_env.unwrapped.get_obs()
 
-                act_low, act_high = agent.train_env.unwrapped.get_action_space()
-                action = np.random.uniform(low=-1.0, high=1.0, size=(1,))
+            act_low, act_high = agent.train_env.unwrapped.get_action_space()
+            action = np.random.uniform(low=-1.0, high=1.0, size=(1,))
 
-                next_state, reward, terminated, truncated, _ = agent.train_env.step(
-                    action
-                )
-                done = terminated or truncated
+            next_state, reward, terminated, truncated, _ = agent.train_env.step(action)
+            done = terminated or truncated
 
-                agent.replay_buffer.push(state, action, reward, next_state, done)
+            agent.replay_buffer.push(state, action, reward, next_state, done)
 
-                agent.update()
+            agent.update()
 
-                agent.total_steps += 1
-                training_steps += 1
+            agent.total_steps += 1
+            training_steps += 1
+
+            progress.update(1)
+
+            if training_steps % self.params["interval"] == 0:
+                eval_results = self.evaluation(agent)
+                id = training_steps // self.params.interval
 
                 with results_lock:
-                    self.results["metadata"]["total_steps"] += 1
+                    if id not in shared_results["rewards"]:
+                        shared_results["rewards"][id] = eval_results
+                    else:
+                        current_results = shared_results["rewards"][id]
+                        shared_results["rewards"][id] = current_results + eval_results
 
-                if training_steps % self.params["interval"] == 0:
-                    results = self.evaluation(agent)
+                progress.set_postfix({"eval": self._get_stats(eval_results)})
 
-                    id = training_steps // self.params["interval"]
-                    with results_lock:
-                        if id not in shared_results["rewards"]:
-                            shared_results["rewards"][id] = results
-                        else:
-                            current_results = shared_results["rewards"][id]
-                            shared_results["rewards"][id] = current_results + results
-
-                    # progress.set_postfix({"eval": self._get_stats(results)})
-
-                if training_steps >= self.params["total_steps"]:
-                    break
-
-        self.save_results()
+            if training_steps >= self.params["total_steps"]:
+                break
