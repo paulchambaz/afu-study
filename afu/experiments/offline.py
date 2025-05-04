fa4d49927451db341c46ebd563539f4064f058f2
Paul Chambaz <paul.chambaz@tutanota.com>
2025-05-04 20:25:27 +0200

    regenerating dataset

--- afu/experiments/offline.py
+++ afu/experiments/offline.py
@@ -1,56 +1,4 @@
 from .base import Experiment
-<<<<<<< HEAD
-import numpy as np
-
-
-class OfflineOnlineTransition(Experiment):
-    def run(self, j, shared_results, results_lock, manager):
-        agent = self.algo(self.hyperparameters)
-
-        dataset = agent.train_env.unwrapped.dataset
-
-        total_samples = len(dataset["observations"])
-        remaining_indices = np.arange(total_samples)
-        np.random.shuffle(remaining_indices)
-
-        training_step = 0
-        offline_step = 0
-
-        for step in range(self.params.offline_steps):
-            batch_size = min(self.params.batch_size, len(remaining_indices))
-
-            batch_indices = remaining_indices[:batch_size]
-            remaining_indices = remaining_indices[batch_size:]
-
-            states = dataset["observations"][batch_indices]
-            actions = dataset["actions"][batch_indices]
-            rewards = dataset["rewards"][batch_indices]
-            next_states = dataset["next_observations"][batch_indices]
-            dones = dataset["terminals"][batch_indices]
-
-            for s, a, r, ns, d in zip(states, actions, rewards, next_states, dones):
-                agent.replay_buffer.push(s, a, r, ns, d)
-
-            agent.update()
-            agent.total_steps += 1
-            offline_step += 1
-            training_step += 1
-
-            if training_step % self.params.offline_interval == 0:
-                results = self.evaluation(agent)
-                id = training_step // self.params.offline_interval
-                with results_lock:
-                    if id not in shared_results["rewards"]:
-                        shared_results["rewards"][id] = results
-                    else:
-                        current_results = shared_results["rewards"][id]
-                        shared_results["rewards"][id] = current_results + results
-
-                # Store the transition point for visualization
-                with results_lock:
-                    if "offline_transition" not in shared_results:
-                        shared_results["offline_transition"] = training_step
-=======
 from tqdm import tqdm
 import pickle
 
@@ -60,10 +8,7 @@ class Offline(Experiment):
         training_step = 0
 
         dataset_files = [
-            "dataset/OnPolicy-AFU-PendulumStudy-v0-data.pk",
-            "dataset/OffPolicy-AFU-PendulumStudy-v0-data.pk",
             "dataset/OnPolicy-SAC-PendulumStudy-v0-data.pk",
-            "dataset/OffPolicy-SAC-PendulumStudy-v0-data.pk",
         ]
 
         self.hyperparameters["dataset_files"] = dataset_files
@@ -101,4 +46,3 @@ class Offline(Experiment):
 
         with results_lock:
             shared_results["agent"] = agent.get_weights()
->>>>>>> 828bb0aa7f23412cbf42a703c2dfc477c7bc17e2
---
+++ afu/experiments/on_policy_dataset.py
@@ -0,0 +1,66 @@
+from .base import Experiment
+from tqdm import tqdm  # type: ignore
+
+
+class OnPolicyDataset(Experiment):
+    def run(self, i, shared_results, results_lock, manager):
+        training_steps = 0
+        agent = self.algo(self.hyperparameters)
+
+        progress = tqdm(
+            range(self.params.total_steps),
+            desc=f"Training {i}/{self.params.n}",
+        )
+
+        transitions = []
+
+        while training_steps < self.params.total_steps:
+            state, _ = agent.train_env.reset()
+
+            while True:
+                action = agent.select_action(state)
+                scaled_action = self._scale_action(action, self.action_space)
+
+                next_state, reward, terminated, truncated, _ = agent.train_env.step(
+                    scaled_action
+                )
+                done = terminated or truncated
+
+                agent.replay_buffer.push(state, action, reward, next_state, done)
+
+                agent.update()
+
+                state = next_state
+                agent.total_steps += 1
+                training_steps += 1
+
+                progress.update(1)
+
+                if training_steps % self.params.interval == 0:
+                    eval_results, eval_transitions = self.evaluation(agent)
+                    transitions.extend(eval_transitions)
+                    id = training_steps // self.params.interval
+
+                    with results_lock:
+                        if id not in shared_results["rewards"]:
+                            shared_results["rewards"][id] = eval_results
+                        else:
+                            current_results = shared_results["rewards"][id]
+                            shared_results["rewards"][id] = (
+                                current_results + eval_results
+                            )
+
+                    progress.set_postfix({"eval": self._get_stats(eval_results)})
+
+                if done:
+                    break
+
+                if training_steps >= self.params.total_steps:
+                    break
+
+            if training_steps >= self.params.total_steps:
+                break
+
+        with results_lock:
+            shared_results["agent"] = agent.get_weights()
+            shared_results["transitions"] = transitions
